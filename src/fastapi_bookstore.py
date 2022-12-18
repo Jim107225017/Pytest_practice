f825edf01cad414e440a5057aea94b7e3da460cb
Jim <jim850212@gmail.com>
2022-12-18 19:19:19 +0800

    [ADD] BookStore CRUD API build by fastapi

--- src/fastapi_bookstore.py
+++ src/fastapi_bookstore.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec 16 13:19:56 2022
+Created on Fri Dec 16 13:40:38 2022
 
 @author: ChiChun.Chen
 """
@@ -8,9 +8,12 @@ Created on Fri Dec 16 13:19:56 2022
 
 # %%
 # Includes Packages
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, ValidationError, validator, root_validator
+from typing import Optional, List
+import uvicorn
 
 
 # %%
@@ -19,14 +22,102 @@ from src.utility import conditions_search
 
 
 # %%
-app = FastAPI()
+description = """
+BookStoreAPI build by FastAPI
 
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name": "create",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "read",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(title="BookStoreAPI", 
+              description=description, 
+              version="0.0.0", 
+              terms_of_service="https://wise-paas.advantech.com/en-us/marketplace/product/advantech.aifs-phm", 
+              contact={"name": "ADVANTECH",
+                       "url": "https://wise-paas.advantech.com/en-us/marketplace/product/advantech.aifs-phm",
+                       "email": "chichun.chen@advantech.com.tw"}, 
+              license_info={"name": "Apache 2.0",
+                            "url": "https://www.apache.org/licenses/LICENSE-2.0.html"}, 
+              docs_url='/v2/docs', 
+              redoc_url='/v2/redoc', 
+              openapi_url='/v2/openapi.json', 
+              openapi_tags=tags_metadata)
+
+
+# %%
+# define pydantic class
 class Book(BaseModel):
     id: int
     title: str
     description: str
-    author: Optional[str]
+    author: str
     borrowed: bool
+
+class Books(BaseModel):
+    # list of dict
+    __root__: List[Book]
+
+class Book_opt(BaseModel):
+    id: int
+    title: Optional[str]
+    description: Optional[str]
+    author: Optional[str]
+    borrowed: Optional[bool]
+
+class Books_opt(BaseModel):
+    # list of dict
+    __root__: List[Book_opt]
+
+class id_duplicate(BaseModel):
+    id_list: list
+    new_id_list: list
+    
+    @root_validator()
+    def id_cannot_duplicate(cls, field_value):
+        id_list = field_value['id_list']
+        new_id_list = field_value['new_id_list']
+        if any(new_id in id_list for new_id in new_id_list):
+            # 自訂義錯誤訊息
+            raise ValueError('ID duplicate error')
+        # 返回傳進來的值
+        return field_value
+    
+class id_exist(BaseModel):
+    id_list: list
+    new_id_list: list
+    
+    @root_validator()
+    def id_need_exist(cls, field_value):
+        id_list = field_value['id_list']
+        new_id_list = field_value['new_id_list']
+        if any(new_id not in id_list for new_id in new_id_list):
+            # 自訂義錯誤訊息
+            raise ValueError('ID not exist error')
+        # 返回傳進來的值
+        return field_value
 
 
 # %%
@@ -51,39 +142,26 @@ books = [
 
 # %%
 # C(create)
-@app.post("/api/books")
-def add_books(book: Book):
+@app.post("/api/books", tags=['create'])
+def add_books(batch_book: Books):
     """ 
     function to add books 
     
     """
     global books
     
-    body_data = book.dict()
-    
-    if body_data == None:
-        msg = "Invalid header"
-        app.logger.error(msg)
-        return msg, 400
-    
-    if not isinstance(body_data, list):
-        msg = "Body type is not array"
-        app.logger.error(msg)
-        return msg, 400
-    
-    RequireKeys = ['id', 'title', 'description', 'author', 'borrowed']
-    # check if format valid
-    if any(key not in book_info.keys() for book_info in body_data for key in RequireKeys):
-        msg = f"Invalid format without required key{RequireKeys}"
-        app.logger.error(msg)
-        return msg, 400
+    body_data = batch_book.dict()['__root__']
     
     id_list = [book['id'] for book in books]
-    # check if id exists
-    if any(book_info['id'] in id_list for book_info in body_data):
-        msg = "Invalid value ID already exists"
-        app.logger.error(msg)
-        return msg, 400
+    new_id_list = [book['id'] for book in body_data]
+    
+    try:
+        id_duplicate(id_list=id_list, new_id_list=new_id_list)
+    except ValidationError as e:
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content=jsonable_encoder(e.errors()),
+            )
     
     books += body_data
     books.sort(key=lambda x: x['id'])
@@ -93,15 +171,27 @@ def add_books(book: Book):
 
 # %%
 # R(read)
-@app.route("/api/books", methods=["GET"])
-def get_books():
+@app.get("/api/books", tags=['read'])
+def get_books(request: Request, 
+              id: Optional[int] = None, 
+              title: Optional[str] = None, 
+              description: Optional[str] = None, 
+              author: Optional[str] = None, 
+              borrowed: Optional[bool] = None):
     """ 
     function to get all books 
+    
+    equal to :
+    id: Union[int, None] = None, 
+    title: Union[str, None] = None, 
+    description: Union[str, None] = None, 
+    author: Union[str, None] = None, 
+    borrowed: Union[bool, None] = None
     
     """
     global books
     
-    condition = request.args.to_dict()   # GET query
+    condition = dict(request.query_params)
     for key in condition.keys():
         if key == 'id':
             condition[key] = int(condition[key])
@@ -112,50 +202,36 @@ def get_books():
                 condition[key] = True
     
     result = [book for book in books if conditions_search(book, condition)]
-    return jsonify({"books": result})
+    return {"books": result}
 
 
 # %%
 # U(update)
-@app.route("/api/books", methods=["PUT"])
-def update_books():
+@app.put("/api/books", tags=['update'])
+def update_books(batch_book: Books_opt):
     """ 
     function to update books 
     
     """
     global books
     
-    body_data = request.get_json()
-    
-    if body_data == None:
-        msg = "Invalid header"
-        app.logger.error(msg)
-        return msg, 400
-    
-    if not isinstance(body_data, list):
-        msg = "Body type is not array"
-        app.logger.error(msg)
-        return msg, 400
-    
-    RequireKeys = ['id']
-    # check if format valid
-    if any(key not in book_info.keys() for book_info in body_data for key in RequireKeys):
-        msg = f"Invalid format without required key{RequireKeys}"
-        app.logger.error(msg)
-        return msg, 400
+    body_data = batch_book.dict()['__root__']
     
     id_list = [book['id'] for book in books]
-    # check if id exists
-    if any(book_info['id'] not in id_list for book_info in body_data):
-        msg = "Invalid value ID not exists"
-        app.logger.error(msg)
-        return msg, 400
+    new_id_list = [book['id'] for book in body_data]
     
-    body_data.sort(reverse=True, key=lambda x: x['id'])
+    try:
+        id_exist(id_list=id_list, new_id_list=new_id_list)
+    except ValidationError as e:
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content=jsonable_encoder(e.errors()),
+            )
+    
     start_idx = 0
+    body_data.sort(reverse=True, key=lambda x: x['id'])
     while body_data:
         book_info = body_data.pop()
-        
         for i in range(start_idx, len(books)):
             if books[i]['id'] != book_info['id']:
                 continue
@@ -164,12 +240,12 @@ def update_books():
             books[i].update(book_info)
             break
     
-    return jsonify('Update books success')
+    return 'Update books success'
 
 
 # %%
 # D(delete)
-@app.route('/api/books/<int:book_id>', methods=['DELETE'])
+@app.delete("/api/books/{book_id}", tags=['delete'])
 def delete_book(book_id: int):
     """ 
     function to delete book 
@@ -178,11 +254,15 @@ def delete_book(book_id: int):
     global books
 
     id_list = [book['id'] for book in books]
-    # check if id exists
-    if book_id not in id_list:
-        msg = "Invalid value ID not exists"
-        app.logger.error(msg)
-        return msg, 400
+    new_id_list = [book_id]
+    
+    try:
+        id_exist(id_list=id_list, new_id_list=new_id_list)
+    except ValidationError as e:
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content=jsonable_encoder(e.errors()),
+            )
     
     for i in range(len(books)):
         if books[i]['id'] != book_id:
@@ -191,9 +271,13 @@ def delete_book(book_id: int):
         books.pop(i)
         break
     
-    return jsonify('Delete book success')
+    return 'Delete book success'
 
 
 # %%
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1234)
+    log_config = uvicorn.config.LOGGING_CONFIG
+    log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+    log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+    uvicorn.run(app, host="0.0.0.0", port=1234, log_config=log_config)
+    # app.run(host='0.0.0.0', port=1234)
